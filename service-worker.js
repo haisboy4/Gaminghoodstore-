b751c80edd152d0f32bac6dec31085654a7ce965
haisboy4 <167570133+haisboy4@users.noreply.github.com>
2025-10-08 17:45:30 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "gaming-hood-store-v1";
+const urlsToCache = [
+  "./",
+  "./index.html"
+];
+
+// Install SW
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch from cache or network
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});￼Enter
